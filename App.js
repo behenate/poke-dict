b33acdc1b476153afe09765056e65ccd6abc0904
behenate <behenate@gmail.com>
2022-06-02 11:23:38 +0200

    Added eslint config, list of evolutions mockup in details tab

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./HomeScreen";
+import DetailsScreen from "./DetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title:'All Pokemon'}}/>
-        <Stack.Screen name="Details" component={DetailsScreen}/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "All Pokemon" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  defaultScreen:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  }
-});
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,30 +1,23 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import PokemonCard from './PokemonCard';
+import * as React from "react";
+import PokemonList from "./PokemonList";
 const DATA = [
-  {name:"evee"},
-  {name:"eveee"},
-  {name:"eveees"},
-  {name:"eveeed"},
-  {name:"eveeef"},
-  {name:"eveeeg"},
-  {name:"eveeeh"}
-
-]
-export default function HomeScreen({navigation}){
-  const renderCard = ({item}) => (
-    <PokemonCard navigation={navigation} name={item.name}/>
-  )
-  return(
-    <FlatList 
-    numColumns='2'
-    data={DATA}
-    renderItem={renderCard}
-    keyExtractor={item=>item.name}>
-    </FlatList>
+  { name: "evee" },
+  { name: "eveee" },
+  { name: "eveees" },
+  { name: "eveeed" },
+  { name: "eveeef" },
+  { name: "eveeeg" },
+  { name: "eveeeh" },
+];
+export default function HomeScreen({ navigation }) {
+  return (
+    // <FlatList
+    // numColumns='2'
+    // data={DATA}
+    // renderItem={renderCard}
+    // keyExtractor={item=>item.name}>
+    // </FlatList>
+    <PokemonList navigation={navigation} data={DATA} />
   );
 }
 
